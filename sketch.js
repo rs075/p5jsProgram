878b26067a9523f8a942606539391f0166bfbd3c
rs075 <119451548+rs075@users.noreply.github.com>
2022-11-29 15:03:41 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,174 @@
+/*
+ * This program sketch is copied from Even Peck's example at
+ * https://editor.p5js.org/evanpeck/sketches/O7MjzPFxb
+ * This is from my own learning.
+ * Reva Sharma
+ * 2022-11-29
+ *
+ * Revisions
+ * 1. 2022-11-29: changed the sound file loading and playing
+ *    The launch audio file is downloaded from
+ *    https://mixkit.co/free-sound-effects/
+ *    which is in mp3 format to be used here.
+ * 2. 2022-11-29: Changed the background of the program
+ * 3. Randomized the color of the text box for every time the
+ *     program runs
+ * 4. Preloaded a new font to be applied to the text in the
+ *     text box and added some new styling to the overall text
+ */
+
+const BOX_WIDTH  = 200;  // textbox dimensions
+const BOX_HEIGHT = 100;
+
+var balls = [];
+var sound;
+var testBall;
+var pressStart
+
+let bg; //variable to represent the background
+
+function preload() {
+
+  sound = loadSound("small-group-cheer-and-applause-518.mp3");  // preload the sound file
+  
+  pressStart = loadFont('PressStart2P-Regular.ttf'); // preload the font
+}
+
+function setup() {
+  
+  bg=loadImage('hot-air-balloon-icon.jpg'); // Load a picture for the background
+//  createCanvas(windowWidth, windowHeight);
+  createCanvas(600,400)
+
+  
+  noStroke();
+  
+  //sound.play();    // play the audio file once
+  sound.loop();  // play the sound file repeatedly
+  
+  for (var ballNum = 0; ballNum < 100; ballNum++) {
+  	balls[ballNum] = new Ball();  
+  }
+
+  let y = height;
+  testBall = new Ball();
+  testBall.size = 50;
+  testBall.ballX = 220;  // if ballX == 225, the ball just slides over the right edge
+  testBall.ballY = 300;
+  testBall.red = 0;
+  testBall.blue = 0;
+  testBall.green = 0;
+  testBall.speedX = 0;
+  testBall.speedY = 1.2;
+}
+
+function createBox() {
+  // prepare a box first
+  strokeWeight(4);
+  rect(0,0,BOX_WIDTH,BOX_HEIGHT);
+  
+  textSize(15);           // size of the text (pixels)
+  fill(0, 102, 153);      // fill() takes R,G,B values as the color
+  // draw the text in the box (x,y,width,height) with the color in fill()
+  stroke(0,0,255);
+  strokeWeight(2);
+  textFont(pressStart);
+  textAlign(CENTER);
+  text('p5js is fun!', BOX_WIDTH/2, BOX_HEIGHT/2);   
+
+}
+
+function draw() {
+
+  background(bg); //Set the background
+  createBox();
+  
+  testBallMove();  //A special ball to test corner collision
+  
+  for (var ballNum = 0; ballNum < balls.length; ballNum++) {
+    balls[ballNum].display();
+    balls[ballNum].checkForHitWall();
+    balls[ballNum].checkForHitBox();
+    balls[ballNum].moveBall();
+    
+    if (mouseIsPressed) {
+      balls[ballNum].randomize()
+    }
+  }
+}
+
+function testBallMove() {
+  
+  testBall.display();
+  testBall.checkForHitWall();
+  testBall.checkForHitBox();
+  testBall.moveBall();
+}
+
+class Ball { // Constructor
+  
+  constructor() {
+    // initial position
+    this.ballX = random(100, width)
+    this.ballY = random(100, height)
+    
+    // Dictates velocity + direction
+    this.speedY = random(-5, 5);
+    this.speedX = random(-5, 5);
+    
+    this.size = random(100);
+    
+    // How transparent the ball is
+    this.alpha = 100
+    
+    // RGB values for color
+    this.red   = random(255);
+    this.green = random(255);
+    this.blue  = random(255)
+  }
+  
+  display() {
+    fill(this.red, this.green, this.blue, this.alpha);
+    ellipse(this.ballX, this.ballY, this.size);
+  }
+  
+  randomize() {
+    this.speedY = random(-5, 5);
+    this.speedX = random(-5, 5);
+  }
+  
+  checkForHitWall() {
+  
+    let radius = this.size / 2;
+    if ((this.ballY+radius) > height || (this.ballY-radius) < 0) {
+  	  this.speedY = -this.speedY;  
+  	}
+    if ((this.ballX+radius) > width  || (this.ballX-radius) < 0) {
+      this.speedX = -this.speedX;  
+    }
+  }
+  
+  checkForHitBox() {
+    
+    let radius = this.size / 2;
+
+//    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT) || d < radius) {
+    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT)) {
+      // bump into the textbox, need to reverse direction
+      this.reverseBall();
+    }
+  }
+  
+  reverseBall() {
+    
+      this.speedX = -this.speedX;
+      this.speedY = -this.speedY;    
+  }
+  
+  moveBall() {
+
+    this.ballX += this.speedX;
+  	this.ballY += this.speedY;
+  }
+  
+}
